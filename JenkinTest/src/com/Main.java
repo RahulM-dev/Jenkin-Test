cd83f9410078de297c71724a5138500fc0b37a17
Rahul Mishra <mishrarahul9861@gmail.com>
2024-12-09 01:09:15 +0530

    Files Changed.

--- JenkinTest/src/com/Main.java
+++ JenkinTest/src/com/Main.java
@@ -7,10 +7,11 @@ public class Main {
 		System.out.println("Printing the pattern..............");
 		for(int i = 0; i < 5; i++) {
 			for(int j = 0; j <= i; j++) {
-				System.out.print("*");
+				System.out.print("* ");
 			}
 			System.out.println();
 		}
+		System.out.println("Printing ended.................");
 	}
 
 }
